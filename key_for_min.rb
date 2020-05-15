3db857454ec68500035cc77990e1ad3aa72a8c12
Spencer Smith <smithspencer817@gmail.com>
2020-05-15 22:15:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_value = name_hash[0]
+  min_value_index = 0
   if name_hash != {}
     name_hash.each do |key, value|
       if name_hash[key] < min_value
-        min_value = value
+        min_value = name_hash[key]
+        min_value_idex = name_hash[key].
       end
     end
-    return min_value
+    return min_value_index
   end
   nil
 end
