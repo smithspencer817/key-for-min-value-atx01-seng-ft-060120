6cc3ff637ca35512b7012644e60d40f29c57c4bc
Spencer Smith <smithspencer817@gmail.com>
2020-05-15 22:36:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,12 +9,12 @@ def key_for_min_value(name_hash)
   if name_hash != {}
     name_hash.each do |key, value|
       if name_hash[key] < min_value
-        binding.pry
         min_value = name_hash[key]
         min_value_key = key
       end
     end
     return min_value_key
+    binding.pry
   end
   nil
 end
