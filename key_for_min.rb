5ae0254dcf1d61dcbc98ad9832a31c514a01b334
Spencer Smith <smithspencer817@gmail.com>
2020-05-15 22:40:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ require "pry"
 
 def key_for_min_value(name_hash)
   min_value = name_hash.first[1]
-  min_value_key = :test
+  min_value_key = name_hash.first[0]
   if name_hash != {}
     name_hash.each do |key, value|
       if name_hash[key] < min_value
